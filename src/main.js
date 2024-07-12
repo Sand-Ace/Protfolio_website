b3696c66c667478d09656ec85a5d950c625ba437
Sand-Ace <sandeshk850@gmail.com>
2024-07-13 00:59:24 +0545

    code cleaned

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import "../styles/modern-normalize.css";
 import "../styles/style.css";
+import "../styles/Components/Header.css";
 import "../styles/utils.css";
 
 // Priority matters here so giving the utils class >> higher priority
